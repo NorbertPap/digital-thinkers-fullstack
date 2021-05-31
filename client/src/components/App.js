de2f1cf16804ba0f383253cc2329672e0c8cfc0c
Norbert <garrote713@gmail.com>
2021-05-31 15:32:00 +0200

    Cocktail page moved to /cocktail

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "../App.css";
-import Cocktail from "./Cocktail";
+import {Route, BrowserRouter as Router} from "react-router-dom";
+import CocktailPage from "../pages/CocktailPage";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Cocktail />
+        <Router>
+          <Route path="/cocktail" component={CocktailPage}/>
+        </Router>
       </div>
     );
   }
---
+++ client/src/pages/CocktailPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Cocktail from "../components/Cocktail";
+
+export default function CocktailPage() {
+  return (
+    <Cocktail/>
+  )
+}
